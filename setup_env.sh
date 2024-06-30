3c93e3411e0b02c18dd620d1292db57179d946e6
Ryan Liang <jiallian@amazon.com>
2024-06-30 06:34:05 +0000

    Add the workspace creation step
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- setup_env.sh
+++ setup_env.sh
@@ -31,7 +31,22 @@ print_failure() {
 # Redirect all output to a log file
 exec > >(tee -i "$LOG_FILE") 2>&1
 
-# 1. Check if git is installed
+# 1. Check if workspace directory exists
+print_header "Checking if workspace directory exists"
+WORKSPACE_DIR="$HOME/workspace"
+if [ -d "$WORKSPACE_DIR" ]; then
+  print_success "workspace directory already exists"
+else
+  echo "workspace directory does not exist. Creating workspace directory..."
+  mkdir -p "$WORKSPACE_DIR"
+  if [ -d "$WORKSPACE_DIR" ]; then
+    print_success "workspace directory successfully created"
+  else
+    print_failure "Failed to create workspace directory"
+  fi
+fi
+
+# 2. Check if git is installed
 print_header "Checking if git is installed"
 if command_exists git; then
   print_success "git is already installed"
@@ -41,7 +56,7 @@ else
   command_exists git && print_success "git successfully installed" || print_failure "git installation failed"
 fi
 
-# 2. Check if zsh is installed
+# 3. Check if zsh is installed
 print_header "Checking if zsh is installed"
 if command_exists zsh; then
   print_success "zsh is already installed"
@@ -51,6 +66,6 @@ else
   command_exists zsh && print_success "zsh successfully installed" || print_failure "zsh installation failed"
 fi
 
-# 3. Switch to zsh and run zsh-specific setup script
+# 4. Switch to zsh and run zsh-specific setup script
 print_header "Switching to zsh to complete setup"
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/RyanL1997/cloud-setup/main/zsh_setup.sh)" || print_failure "zsh-specific setup script failed"
